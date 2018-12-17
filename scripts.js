f6a91f386742603e5f14d610c30c127534226434
mazimpakar <rosemazimpaka2@gmail.com>
2018-12-17 10:59:17 +0200

    add link of triangle shape

--- scripts.js
+++ scripts.js
@@ -4,14 +4,21 @@ function getResult(){
     var sideC=parseFloat(document.getElementById('sideC').value);
 if(sideA===sideB && sideB===sideC){
     alert ("This is an equilateral triangle");
-
+    window.location.href="equilateral.html";
 }
-else if (sideA===sideB || sideA===sideC || sideB===sideC)
+else if (sideA===sideB || sideA===sideC || sideB===sideC){
     alert ("This is an iscocele triangle");
+    window.location.href="iscocele.html";
+
 }
 else if ((sideA + sideB) <= sideC || (sideA + sideC) <= sideB || (sideB + sideC) <= sideA){
     alert("Not a definite triangle");
+    window.location.href="not triangle.html";
+
 }
 else{ 
      alert("This is a scalene triangle");
+     window.location.href="scalene.html";
+
+}
 }
