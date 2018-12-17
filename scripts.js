ef615628bd015c7514db8152253ff1be7374773a
mazimpakar <rosemazimpaka2@gmail.com>
2018-12-17 09:29:30 +0200

    add alert of iscocele

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ function getResult(){
     var sideC=parseFloat(document.getElementById('sideC').value);
 if(sideA===sideB && sideB===sideC){
     alert ("This is an equilateral triangle");
-    
+
 }
-else if (sideA===sideB || sideA===sideC || sideB===sideC){
-    alert ("This is an isoscle triangle");
+else if (sideA===sideB || sideA===sideC || sideB===sideC)
+alert ("This is an isoscle triangle");
 }
 else if ((sideA + sideB) <= sideC || (sideA + sideC) <= sideB || (sideB + sideC) <= sideA){
     alert("Not a definite triangle");
